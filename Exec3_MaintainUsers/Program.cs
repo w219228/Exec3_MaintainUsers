ce5134ae204d1a69ee5c2f3235d5cc5420a1fb0a
w2192 <w2192@LAPTOP-QEVPLLMQ>
2022-11-14 23:17:56 +0800

    c

---
+++ Exec3_MaintainUsers/Program.cs
@@ -0,0 +1,37 @@
+﻿using ISpan.Utility;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec3_MaintainUsers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var dbHelper = new SqlDbHelper("default");
+			string sql = "SELECT Id, Title FROM News WHERE Id> @Id  ORDER BY Id ASC";
+			Console.WriteLine(sql);
+			try
+			{
+				var parameters = new SqlParameterBuilder().AddNInt("id", 0).Build();
+				DataTable news = dbHelper.Select(sql, parameters);
+				foreach (DataRow row in news.Rows)
+				{
+					int id = row.Field<int>("id");
+					string title = row.Field<string>("title");
+					Console.WriteLine($"Id={id}, Title={title}");
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"連線失敗, 原因 :{ex.Message}");
+			}
+
+		}
+	}
+}
---
+++ ISpan.Utility/SqlParameterBuilder.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISpan.Utility
+{
+	public class SqlParameterBuilder
+	{
+		private List<SqlParameter> parameters = new List<SqlParameter>();
+
+		public SqlParameterBuilder AddNVarchar(string name,
+												int length,
+												string value)
+		{
+			var param = new SqlParameter(name,
+										SqlDbType.NVarChar,
+										length)
+			{ Value = value };
+			parameters.Add(param);
+
+			return this;
+		}
+
+		public SqlParameterBuilder AddNInt(string name, int value)
+		{
+			var param = new SqlParameter(name, SqlDbType.Int) { Value = value };
+			parameters.Add(param);
+
+			return this;
+		}
+
+		public SqlParameterBuilder AddNDateTime(string name, DateTime value)
+		{
+			var param = new SqlParameter(name, SqlDbType.DateTime) { Value = value };
+			parameters.Add(param);
+
+			return this;
+		}
+
+		public SqlParameterBuilder AddNBool(string name, bool value)
+		{
+			var param = new SqlParameter(name, SqlDbType.Bit) { Value = value };
+			parameters.Add(param);
+
+			return this;
+		}
+
+		public SqlParameter[] Build()
+		{
+			return parameters.ToArray();
+		}
+	}
+}
